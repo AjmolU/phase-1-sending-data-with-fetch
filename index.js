51b8cd87903be9092701928d1d30b3e05502604a
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-08-22 13:59:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function configurationObject(data) {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+};
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", configurationObject({name: userName, email: userEmail}))
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+      document.body.append(object.id);
+  })
+  .catch(function(error) {
+      document.body.append(error.message);
+  });
+}
